81b8ac30824fdaa7bea079935a2212c1ab1a0b2e
Unknown <wesleyfinck@gmail.com>
2018-07-06 14:13:56 -0300

    added client script for myo band

---
+++ Assets/Scripts/MyoReaderClient.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Threading;
+using UnityEngine.Networking;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System;
+
+public class MyoReaderClient : MonoBehaviour {
+
+
+    private string host = "127.0.0.1";
+    private int port = 12345;
+    private float leftReading;
+    private float rightReading;
+
+    TcpClient socketClient;
+
+    // Use this for initialization
+    void Start () {
+        ConnectSocket();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ConnectSocket()
+    {
+        IPAddress ipAddress = IPAddress.Parse(host);
+
+        socketClient = new TcpClient();
+        try
+        {
+            socketClient.Connect(ipAddress, port);
+        }
+
+        catch
+        {
+            Debug.Log("error when connecting to server socket");
+        }
+    }
+}
