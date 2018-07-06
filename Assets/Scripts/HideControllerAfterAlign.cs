c9e9f9db2bf9b6a19b3b182eff23bf81cf1f9651
Unknown <wesleyfinck@gmail.com>
2018-07-06 11:10:39 -0300

    finished script to make controller disapear

--- Assets/Scripts/HideControllerAfterAlign.cs
+++ Assets/Scripts/HideControllerAfterAlign.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class HideControllerAfterAlign : MonoBehaviour {
 
-	// Use this for initialization
+    private Renderer mesh;
+
+    // Use this for initialization
 	void Start () {
-		
+        mesh = GetComponent<Renderer>();
+        mesh.enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown("space"))
+        {
+            mesh.enabled = false;
+        }
 	}
 }
