3d6f3d6eb2998e13d5d368a6e4d642d88899e83e
Unknown <wesleyfinck@gmail.com>
2018-07-06 09:36:28 -0300

    added script for pick up instantiation
    added script that will instantiate the pick up prefab, same script as PickUpsHoloLens project from May

---
+++ Assets/Scripts/InstantiatorController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiatorController : MonoBehaviour {
+
+    public GameObject pickupPrefab; //for the actual prefab
+    GameObject pickupPrefabClone; //for the clone
+
+    void Start()
+    {
+        InstantiatePrefab();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            ResetPrefab();
+        }
+    }
+
+    void ResetPrefab()
+    {
+        Destroy(pickupPrefabClone);
+        InstantiatePrefab();
+
+    }
+
+    void InstantiatePrefab()
+    {
+        pickupPrefabClone = Instantiate(pickupPrefab, transform.position, Quaternion.identity) as GameObject;
+        pickupPrefabClone.transform.parent = transform.parent;
+        //pickupPrefabClone.transform.localScale = new Vector3(0.3967236f, 0.3967236f, 0.3967236f);
+    }
+}
