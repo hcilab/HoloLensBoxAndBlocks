935776389af031ce4fdb0547a86c31a144169eb0
Unknown <wesleyfinck@gmail.com>
2018-07-10 17:30:53 -0300

    adding more functionality to the voice recognizer script

--- Assets/Scripts/VoiceManager.cs
+++ Assets/Scripts/VoiceManager.cs
@@ -6,21 +6,43 @@ using System.Linq;
 
 public class VoiceManager : MonoBehaviour {
 
+    public GameObject controller;
+
+    OffsetFix offsetFix;
 
     KeywordRecognizer keywordRecognizer;
     Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
 
     // Use this for initialization
 	void Start () {
+
+
+
         keywords.Add("align", () =>
         {
             //OffsetFix.AlignAxes();
         });
 
+        //create the keyword recognizer and tell it what to recognize
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        //register for the OnPhraseRecognized event
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        // if the keyword recognized is in our dictionary, call that Action
+        if(keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
 }
