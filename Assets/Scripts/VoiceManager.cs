afc88b6a84d68882c1a221a8c7ed4e5be3134b3f
Unknown <wesleyfinck@gmail.com>
2018-07-10 17:19:13 -0300

    creating a script to manage voice inputs

---
+++ Assets/Scripts/VoiceManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class VoiceManager : MonoBehaviour {
+
+
+    KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+	void Start () {
+        keywords.Add("align", () =>
+        {
+            //OffsetFix.AlignAxes();
+        });
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
