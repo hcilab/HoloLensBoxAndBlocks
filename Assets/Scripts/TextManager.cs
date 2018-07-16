a7f0bf7bf4e69493528d7067cb29c43febcee058
Unknown <wesleyfinck@gmail.com>
2018-07-16 15:51:35 -0300

    initialize gamestate enum in start method

--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -12,7 +12,7 @@ public class TextManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        gameState = GameState.ReadyToScan;
+        gameState = GameState.RoomScan;
     }
 
     // Update is called once per frame
