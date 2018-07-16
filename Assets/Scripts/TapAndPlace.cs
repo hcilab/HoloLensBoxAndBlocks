8f7868b916efefb2f95e1d6baa2ab729572ce3b9
Unknown <wesleyfinck@gmail.com>
2018-07-16 13:55:22 -0300

    added code to tapandplace script

--- Assets/Scripts/TapAndPlace.cs
+++ Assets/Scripts/TapAndPlace.cs
@@ -1,11 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HoloToolkit.Unity.InputModule;
 
-public class TapAndPlace : MonoBehaviour {
+public class TapAndPlace : MonoBehaviour, IInputClickHandler
+{
+    public GameObject BoxAndBlocks;
 
-	// Use this for initialization
-	void Start () {
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        float yCamRot = Camera.main.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, yCamRot, 0);
+        Instantiate(BoxAndBlocks, transform.parent.position, rotation);
+        DestroyAllPlaceableMarkers();
+    }
+
+    private void DestroyAllPlaceableMarkers()
+    {
+        GameObject[] placeables = GameObject.FindGameObjectsWithTag("placeable");
+        foreach (GameObject placeable in placeables)
+        {
+            Destroy(placeable);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
