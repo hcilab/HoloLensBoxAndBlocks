395a30c85a1b03fc075abb3c24b90a98f27f3fb0
Unknown <wesleyfinck@gmail.com>
2018-08-02 14:57:44 -0300

    added a header

--- Assets/Scripts/BoxCounter.cs
+++ Assets/Scripts/BoxCounter.cs
@@ -17,7 +17,9 @@ public class BoxCounter : MonoBehaviour {
     InstantiatorController instantiatorController;
     TextManager textManagerScript;
 
-    // Use this for initialization
+    /// <summary>
+    /// 
+    /// </summary>
     void Start () {
         textManagerObject = GameObject.Find("TextManager");
         textManagerScript = textManagerObject.GetComponent<TextManager>();
@@ -25,7 +27,13 @@ public class BoxCounter : MonoBehaviour {
         instantiatorController = instantiator.GetComponent<InstantiatorController>();
         boxCount = 0;
     }
-	
+    /// <summary>
+    /// is called when another object enters the trigger. Checks whether the object is a pickup and if game is
+    /// in the TimerStarted game state. If both conditions are met, boxCount increments and is used to count
+    /// the number of blocks successfully moved over during the 60 second test. A new pickup prefab is instantiated
+    /// everytime a blocks is successfully moved over. This is to ensure there are always the same amount of blocks 
+    /// on the side to pick them up from.
+    /// </summary>
     private void OnTriggerEnter(Collider other)
     {
         if ((other.gameObject.tag == "pickup") && (textManagerScript.gameState == GameState.TimerStarted))
