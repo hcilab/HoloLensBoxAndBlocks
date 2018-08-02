8df07277a103a67182362908f5e0dcbd27390043
Unknown <wesleyfinck@gmail.com>
2018-08-02 16:28:03 -0300

    added headers to pickupmanager

--- Assets/Scripts/PickUpManager.cs
+++ Assets/Scripts/PickUpManager.cs
@@ -15,6 +15,9 @@ public class PickUpManager : MonoBehaviour {
     private HandAnimatorManager handAnimatorManager;
 
     // Use this for initialization
+    /// <summary>
+    /// makes an instance of the HandAnimatorManager script component of ar_hand
+    /// </summary>
     void Start()
     {
         hand = GameObject.Find("ar_hand");
@@ -22,6 +25,10 @@ public class PickUpManager : MonoBehaviour {
     }
 
     // Update is called once per frame
+    /// <summary>
+    /// checks if the pickup is being picked up by the hand, if it is, the pick up is made kinematic and a child of the hand.
+    /// when it is no longer being picked up, it falls and stops being a child of the hand.
+    /// </summary>
     void Update()
     {
         if (IsPickedUp() && !attached)
@@ -41,6 +48,9 @@ public class PickUpManager : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// checks to see if pick up is in contact with a thumb and a finger
+    /// </summary>
     public bool IsPickedUp()
     {
         return (touchingFinger && touchingThumb);
